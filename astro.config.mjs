967abbd4d7ba35e8bdb24c28dbd2aafc1249c81d
Miguel Palencia-Olivar <miguel.palencia-olivar@cognitive-works.com>
2026-04-30 21:59:44 +0200

    fixed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ira-un.github.io',
-  base: '/iraun',
+  base: '/iraun.github.io',
 });
